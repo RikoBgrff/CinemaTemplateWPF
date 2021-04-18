a50bcc89f154ddcb635a8f579b3c25addf8d160f
Bagirli Arifali Azad <Bagi_mz73@itstep.edu.az>
2021-04-18 16:43:17 +0400

    Add project files.

---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public dynamic Data { get; set; }
+        HttpClient http = new HttpClient();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            var name = movieTextBox.Text;
+            response = http.GetAsync($@"http://www.omdbapi.com/?apikey=e4d8a8d9&s={name}&plot=full").Result;
+            var str = response.Content.ReadAsStringAsync().Result;
+            Data = JsonConvert.DeserializeObject(str);
+            movieImage.Source = Data.Search[0].Poster;
+            movieLabel.Content = Data.Search[0].Title;
+        }
+            catch (Exception)
+            {
+
+                MessageBox.Show("not found qaqash direshme");
+                    }
+}
+    }
+}
